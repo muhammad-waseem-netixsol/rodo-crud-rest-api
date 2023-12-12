9769be3d22afc9d58cd64e7af19aa56137676f0d
muhammad-waseem-netixsol <151015897+muhammad-waseem-netixsol@users.noreply.github.com>
2023-12-12 12:25:34 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const app = express();
 
 // Body parser middleware
 app.use(bodyParser.json());
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 // Routes
 app.get('/', (req, res) => {
   res.send('Hello, Express!');
